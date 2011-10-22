64af37f0148a6c2699151dd2c444843a45a45d44
Alexadre Michetti Manduca <a.michetti@gmail.com>
2011-10-22 13:59:21 -0200

    Add app.js process CPU and Memory utilization to log message

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 var io = require('socket.io').listen(3000);
+var exec = require('child_process').exec; 
 
 io.configure(function() {
   io.set('log level', 1);
 });
 
+// command to read process consumed memory and cpu time
+var getCpuCommand = "ps -p " + process.pid + " -u | grep " + process.pid;
+
 var users = 0;
 var count = 0;
 
+function roundNumber(num, precision) {
+  return parseFloat(Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision));
+}
+
 setInterval(function() {
-  var aux = Math.round(count / users)
+  var aux = roundNumber(count / users, 2)
 
-  var msu = (users > 0 ? aux  : 0);
-  var trm = (users > 0 ? Math.round(1000 / aux) : '-');
+  var msu = (users > 0 ? aux : 0);
+  var trm = (users > 0 ? roundNumber(1000 / aux, 2) : '-');
 
-  var l = [
-    'U: ' + users,
-    'M/S: ' + count,
-    'M/S/U: ' + msu,
-    'RT: ' + trm,
-  ];
+  // call a system command (ps) to get current process resources utilization
+  var child = exec(getCpuCommand, function(error, stdout, stderr) {
+    var s = stdout.split(/\s+/);
+    var cpu = s[2];
+    var memory = s[3];
 
-  console.log(l.join(', '));
-  count = 0;
+    var l = [
+      'U: ' + users,
+      'M/S: ' + count,
+      'M/S/U: ' + msu,
+      'RT: ' + trm,
+      'CPU: ' + cpu,
+      'Mem: ' + memory
+    ];
+
+    console.log(l.join(', '));
+    count = 0;
+  });
 
 }, 1000);
 
